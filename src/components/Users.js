0a3f299ce151bd13e9709f4291a2963c59bdb349
Janae Quinones <janaemassa@gmail.com>
2021-06-08 20:37:13 -0400

    commit tree 9a05f9be1b17c7f25ed276204d44121ccb9cf6de

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,26 +1,33 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// add any needed imports here
 class Users extends Component {
-  renderUsers = () => {
-    return this.props.users.map((user) => {
-      return <li key={user.username}>{user.username}</li>;
-    });
-  };
+
   render() {
+    // debugger
     return (
       <div>
         <ul>
-          {this.props.userCount} Users!
-          {this.renderUsers()}
+          Users!
+          {/* Write code here that displays the usernames of all users in the Redux store */}
+          {this.props.users.map((user, i) => (
+            <li key={i}>{user.username}</li>
+          ))}
+          {/* In addition, display the total number of users curently in the store */}
         </ul>
+        <h2>Amount of users in Store: {this.props.userCount}</h2>
       </div>
-    );
+    )
   }
 }
 
+//add mapStateToProps here
 const mapStateToProps = (state) => {
-  return { users: state.users, userCount: state.users.length };
-};
-
+  // debugger
+  return { 
+    users: state.users,
+    userCount: state.users.length
+  }
+}
 // connect this component to Redux
-export default connect(mapStateToProps)(Users);
+export default connect(mapStateToProps)(Users)
--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
 
-import manageUsers from "./reducers/manageUsers";
+import manageUsers from './reducers/manageUsers'
+
 
 // add imports and code
-import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
-const store = createStore(
-  manageUsers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(manageUsers)
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={store} >
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
